4214c477e47cc884407867c1f177cd0f225aa5c0
Tyler Sedlar <sedlarizona@gmail.com>
2015-10-31 15:10:42 -0700

    Fixed instruction opcode parsing.

--- src/core/parsers/BytecodeInstructions.js
+++ src/core/parsers/BytecodeInstructions.js
@@ -1,43 +1,53 @@
 import * as _ from 'lodash';
 import {NiceBuffer} from './NiceBuffer';
+import {JVMCodeAttribute} from '../jvm/Attributes'
 
 export const OPCODE_TO_NAME = [
-  'NOP', 'ACONST_NULL', 'ICONST_M1', 'ICONST_0',
-  'ICONST_1', 'ICONST_2', 'ICONST_3', 'ICONST_4',
-  'ICONST_5', 'LCONST_0', 'LCONST_1', 'FCONST_0',
-  'FCONST_1', 'FCONST_2', 'DCONST_0', 'DCONST_1',
-  'BIPUSH', 'SIPUSH', 'LDC', '', '', 'ILOAD',
-  'LLOAD', 'FLOAD', 'DLOAD', 'ALOAD', '', '', '',
-  '', '', '', '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', 'IALOAD', 'LALOAD', 'FALOAD',
-  'DALOAD', 'AALOAD', 'BALOAD', 'CALOAD', 'SALOAD',
-  'ISTORE', 'LSTORE', 'FSTORE', 'DSTORE', 'ASTORE',
-  '', '', '', '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '', 'IASTORE',
-  'LASTORE', 'FASTORE', 'DASTORE', 'AASTORE', 'BASTORE',
-  'CASTORE', 'SASTORE', 'POP', 'POP2', 'DUP', 'DUP_X1',
-  'DUP_X2', 'DUP2', 'DUP2_X1', 'DUP2_X2', 'SWAP',
-  'IADD', 'LADD', 'FADD', 'DADD', 'ISUB', 'LSUB',
-  'FSUB', 'DSUB', 'IMUL', 'LMUL', 'FMUL', 'DMUL',
-  'IDIV', 'LDIV', 'FDIV', 'DDIV', 'IREM', 'LREM',
-  'FREM', 'DREM', 'INEG', 'LNEG', 'FNEG', 'DNEG',
-  'ISHL', 'LSHL', 'ISHR', 'LSHR', 'IUSHR', 'LUSHR',
-  'IAND', 'LAND', 'IOR', 'LOR', 'IXOR', 'LXOR',
-  'IINC', 'I2L', 'I2F', 'I2D', 'L2I', 'L2F', 'L2D',
-  'F2I', 'F2L', 'F2D', 'D2I', 'D2L', 'D2F', 'I2B',
-  'I2C', 'I2S', 'LCMP', 'FCMPL', 'FCMPG', 'DCMPL',
-  'DCMPG', 'IFEQ', 'IFNE', 'IFLT', 'IFGE', 'IFGT',
-  'IFLE', 'IF_ICMPEQ', 'IF_ICMPNE', 'IF_ICMPLT',
-  'IF_ICMPGE', 'IF_ICMPGT', 'IF_ICMPLE', 'IF_ACMPEQ',
-  'IF_ACMPNE', 'GOTO', 'JSR', 'RET', 'TABLESWITCH',
-  'LOOKUPSWITCH', 'IRETURN', 'LRETURN', 'FRETURN',
-  'DRETURN', 'ARETURN', 'RETURN', 'GETSTATIC',
-  'PUTSTATIC', 'GETFIELD', 'PUTFIELD', 'INVOKEVIRTUAL',
-  'INVOKESPECIAL', 'INVOKESTATIC', 'INVOKEINTERFACE',
-  'INVOKEDYNAMIC', 'NEW', 'NEWARRAY', 'ANEWARRAY',
-  'ARRAYLENGTH', 'ATHROW', 'CHECKCAST', 'INSTANCEOF',
-  'MONITORENTER', 'MONITOREXIT', '', 'MULTIANEWARRAY',
-  'IFNULL', 'IFNONNULL'
+  'NOP', 'ACONST_NULL', 'ICONST_M1', 'ICONST_0', 
+  'ICONST_1', 'ICONST_2', 'ICONST_3', 'ICONST_4', 
+  'ICONST_5', 'LCONST_0', 'LCONST_1', 'FCONST_0', 
+  'FCONST_1', 'FCONST_2', 'DCONST_0', 'DCONST_1', 
+  'BIPUSH', 'SIPUSH', 'LDC', 'LDC_W', 'LDC2_W', 
+  'ILOAD', 'LLOAD', 'FLOAD', 'DLOAD', 'ALOAD', 
+  'ILOAD_0', 'ILOAD_1', 'ILOAD_2', 'ILOAD_3', 
+  'LLOAD_0', 'LLOAD_1', 'LLOAD_2', 'LLOAD_3', 
+  'FLOAD_0', 'FLOAD_1', 'FLOAD_2', 'FLOAD_3', 
+  'DLOAD_0', 'DLOAD_1', 'DLOAD_2', 'DLOAD_3', 
+  'ALOAD_0', 'ALOAD_1', 'ALOAD_2', 'ALOAD_3', 
+  'IALOAD', 'LALOAD', 'FALOAD', 'DALOAD', 'AALOAD', 
+  'BALOAD', 'CALOAD', 'SALOAD', 'ISTORE', 'LSTORE', 
+  'FSTORE', 'DSTORE', 'ASTORE', 'ISTORE_0', 
+  'ISTORE_1', 'ISTORE_2', 'ISTORE_3', 'LSTORE_0', 
+  'LSTORE_1', 'LSTORE_2', 'LSTORE_3', 'FSTORE_0', 
+  'FSTORE_1', 'FSTORE_2', 'FSTORE_3', 'DSTORE_0', 
+  'DSTORE_1', 'DSTORE_2', 'DSTORE_3', 'ASTORE_0', 
+  'ASTORE_1', 'ASTORE_2', 'ASTORE_3', 'IASTORE', 
+  'LASTORE', 'FASTORE', 'DASTORE', 'AASTORE', 
+  'BASTORE', 'CASTORE', 'SASTORE', 'POP', 'POP2', 'DUP', 
+  'DUP_X1', 'DUP_X2', 'DUP2', 'DUP2_X1', 'DUP2_X2', 'SWAP', 
+  'IADD', 'LADD', 'FADD', 'DADD', 'ISUB', 'LSUB', 'FSUB', 
+  'DSUB', 'IMUL', 'LMUL', 'FMUL', 'DMUL', 'IDIV', 'LDIV', 
+  'FDIV', 'DDIV', 'IREM', 'LREM', 'FREM', 'DREM', 'INEG', 
+  'LNEG', 'FNEG', 'DNEG', 'ISHL', 'LSHL', 'ISHR', 'LSHR', 
+  'IUSHR', 'LUSHR', 'IAND', 'LAND', 'IOR', 'LOR', 'IXOR', 
+  'LXOR', 'IINC', 'I2L', 'I2F', 'I2D', 'L2I', 'L2F', 'L2D', 
+  'F2I', 'F2L', 'F2D', 'D2I', 'D2L', 'D2F', 'I2B', 'I2C', 
+  'I2S', 'LCMP', 'FCMPL', 'FCMPG', 'DCMPL', 'DCMPG', 'IFEQ',
+   'IFNE', 'IFLT', 'IFGE', 'IFGT', 'IFLE', 'IF_ICMPEQ', 
+   'IF_ICMPNE', 'IF_ICMPLT', 'IF_ICMPGE', 'IF_ICMPGT', 
+   'IF_ICMPLE', 'IF_ACMPEQ', 'IF_ACMPNE', 'GOTO', 'JSR', 
+   'RET', 'TABLESWITCH', 'LOOKUPSWITCH', 'IRETURN', 'LRETURN', 
+   'FRETURN', 'DRETURN', 'ARETURN', 'RETURN', 'GETSTATIC', 
+   'PUTSTATIC', 'GETFIELD', 'PUTFIELD', 'INVOKEVIRTUAL', 
+   'INVOKESPECIAL', 'INVOKESTATIC', 'INVOKEINTERFACE', 
+   'INVOKEDYNAMIC', 'NEW', 'NEWARRAY', 'ANEWARRAY', 
+   'ARRAYLENGTH', 'ATHROW', 'CHECKCAST', 'INSTANCEOF', 
+   'MONITORENTER', 'MONITOREXIT', 'WIDE', 'MULTIANEWARRAY', 
+   'IFNULL', 'IFNONNULL', 'GOTO_W', 'JSR_W', 'BREAKPOINT', 
+   '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 
+   '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 
+   '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 
+   '', '', '', '', '', '', 'IMPDEP1', 'IMPDEP2'
 ];
 
 export const NAME_TO_OPCODE = _.invert(OPCODE_TO_NAME);
@@ -270,30 +280,27 @@ export function createInstruction(idx, opcode, wide) {
 }
 
 export function injectInstructions(method) {
-  let code = _.find(method.attribute_info, { name: 'Code' });
+  let attr = _.find(method.attribute_info, { name: 'Code' });
   let wide = false;
-  let buffer = new NiceBuffer(new Buffer(code.info.data));
-  code.instructions = [];
+  let data = new JVMCodeAttribute();
+  let bytes = data.decode(attr);
+  let code = bytes.code;
+  let buffer = new NiceBuffer(new Buffer(code));
+  attr.instructions = [];
   let current = null;
-  while (buffer.pos < code.info.data.length) {
-    // let previous = current;
-    let idx = code.instructions.length;
+  while (buffer.pos < code.length) {
+    let previous = current;
+    let idx = attr.instructions.length;
     let opcode = buffer.byte();
-    console.log(OPCODE_TO_NAME[opcode]);
     current = createInstruction(idx, opcode, wide);
     current.read(buffer);
-    // if (previous != null) {
-    //   // current.previous = previous;
-    //   // previous.next = current;
-    // }
+    if (previous != null) {
+      current.previous = previous;
+      previous.next = current;
+    }
     wide = (opcode === NAME_TO_OPCODE['WIDE']);
-    code.instructions.push(current);
+    attr.instructions.push(current);
   }
-  // code.instructions = _.map(code.info.data, (opcode, idx) => {
-  //   let instruction = createInstruction(idx, opcode, wide);
-  //   wide = (opcode === NAME_TO_OPCODE['WIDE']);
-  //   return instruction;
-  // });
 
   return method;
 }
