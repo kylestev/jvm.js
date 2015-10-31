a17e99310e42aedd7795be769c7fd2c8a38eefff
Tyler Sedlar <sedlarizona@gmail.com>
2015-10-31 15:12:37 -0700

    fixed deep nesting

--- src/core/jvm/insn/VariableInstruction.js
+++ src/core/jvm/insn/VariableInstruction.js
@@ -13,13 +13,11 @@ export default class VariableInstruction extends AbstractInstruction {
   get var() {
     if (this.instruction.constructor.name === 'ImmediateByteInstruction') {
       return this.instruction.val;
-    } else {
-      if (this.instruction.opname.includes('M1')) {
-          return -1;
-      } else if (this.instruction.opname.includes('_')) {
-          return parseInt(_.last(this.instruction.opname.split('_')));
-      }
-      return 0;
+    } else if (this.instruction.opname.includes('M1')) {
+        return -1;
+    } else if (this.instruction.opname.includes('_')) {
+        return parseInt(_.last(this.instruction.opname.split('_')));
     }
+    return 0;
   }
 }
