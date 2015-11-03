11210555d7dabe2588e1aac90718a4ff9a4a8867
Kyle Stevenson <kyle@kylestevenson.me>
2015-11-03 11:28:35 -0800

    fix bug in ClassLoader where pool was not passed to ClasssInfo

--- src/core/ClassLoader.js
+++ src/core/ClassLoader.js
@@ -61,7 +61,7 @@ class ClassLoader {
         let version = { major: cls.major_version, minor: minor_version };
         let className = constantPoolLookup(pool, pool[cls.this_class - 1].info.name_index);
         let superName = constantPoolLookup(pool, pool[cls.super_class - 1].info.name_index);
-        let classInfo = new ClassInfo(cls.access_flags, version, className, superName);
+        let classInfo = new ClassInfo(cls.access_flags, version, className, superName, pool);
 
         _.each(cls.interfaces, (intr) => {
           let inter = constantPoolLookup(pool, pool[intr.class_index - 1].info.name_index);
