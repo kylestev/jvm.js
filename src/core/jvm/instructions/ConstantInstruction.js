f563c580a754bdcc21506b97b83b32ab54c35e5f
Kyle Stevenson <kyle@kylestevenson.me>
2015-11-07 15:32:55 -0800

    add ConstantInstruction value lookup

--- src/core/jvm/instructions/ConstantInstruction.js
+++ src/core/jvm/instructions/ConstantInstruction.js
@@ -1,9 +1,36 @@
+import * as _ from 'lodash';
+import { NUMERIC_TAGS, TAG_STRING } from '../ConstantPool';
+
 /** @ignore */
 const InstructionWrapper = require('./InstructionWrapper');
+
+function isNumeric(tag) {
+  return _.includes(NUMERIC_TAGS, tag);
+}
 
 export default class ConstantInstruction extends InstructionWrapper {
 
   constructor(instruction) {
     super(instruction);
+  }
+
+  isNumeric() {
+    return _.includes(NUMERIC_TAGS, this.tag);
+  }
+
+  isString() {
+    return this.tag === TAG_STRING;
+  }
+
+  get poolInfo() {
+    return this.pool.at(this.instruction.val);
+  }
+
+  get tag() {
+    return this.poolInfo.tag;
+  }
+
+  get val() {
+    return this.pool.valueAt(this.instruction.val);
   }
 }
