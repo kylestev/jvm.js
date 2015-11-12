43b8e86875861e9112a39e1e923c5d74348857d6
Kyle Stevenson <kyle@kylestevenson.me>
2015-11-12 14:33:51 -0800

    add MethodInstruction#parameters

--- src/core/jvm/instructions/MethodInstruction.js
+++ src/core/jvm/instructions/MethodInstruction.js
@@ -1,9 +1,19 @@
 /** @ignore */
 const MemberInstruction = require('./MemberInstruction');
 
+import { parameterParser } from '../MethodInfo';
+
 export default class MethodInstruction extends MemberInstruction {
 
   constructor(methodInfo, idx, opcode) {
     super(methodInfo, idx, opcode);
+  }
+
+  /**
+   * Slices the parameters from the method this instruction refers to.
+   * @return {Array<string>} method parameter types
+   */
+  get parameters() {
+    return parameterParser(this.desc);
   }
 }
