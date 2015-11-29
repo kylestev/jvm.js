a6855b79576af2a77f59229cd87a35c0e90ee4d2
Kyle Stevenson <kyle@kylestevenson.me>
2015-11-29 05:22:34 -0800

    add IoC container

---
+++ src/ioc/IoC.js
@@ -0,0 +1,47 @@
+const _ = require('lodash');
+
+class Container {
+  constructor() {
+    this.bindings = {};
+  }
+
+  exists(name, assert = false) {
+    let exists = name in this.bindings;
+
+    if ( ! exists && assert) {
+      throw new Error('Whoa, ' + name + ' has not been bound to this container!');
+    }
+
+    return exists;
+  }
+
+  factory(cls) {
+    let key = cls + 'Factory';
+    this.exists(key, true);
+
+    let Factory = this.bindings[key];
+    return function () {
+      let args = _.values(arguments);
+      return new (Function.prototype.bind.apply(Factory, args));
+    }
+  }
+
+  make(name, params = []) {
+    this.exists(name, true);
+    return this.bindings[name].apply(null, params);
+  }
+
+  set(name, factory, overwrite = false) {
+    if (name in this.bindings && ! overwrite) {
+      return;
+    }
+
+    this.bindings[name] = factory;
+  }
+}
+
+let IoC = new Container;
+
+export {
+  IoC
+};
