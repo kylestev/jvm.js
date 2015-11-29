31a1ecf25a9368ca7491e2a436da2b10c6e8ecd1
Kyle Stevenson <kyle@kylestevenson.me>
2015-11-29 06:43:12 -0800

    export static IoC instance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { IoC } from './ioc/IoC';
 import { Jar } from './core/Jar';
 import { ClassVisitor, VerboseClassVisitor } from './visitor/ClassVisitor';
 
 export {
+  IoC,
   Jar,
   ClassVisitor,
   VerboseClassVisitor
